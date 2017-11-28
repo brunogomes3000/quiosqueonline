91966220ca6e874bfda83869734542049ba1c86c
thainapc <thainapadilha1@gmail.com>
2017-11-28 11:01:45 -0300

    Add Classe Usuario em models.py e Edit admin.py

--- core/models.py
+++ core/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Usuario(models.Model):
+	email= models.CharField('E-mail',primary_key=True, max_length=30)
+	senha= models.CharField('Senha', max_length=16)
+	cpfCpnj= models.CharField('CPF', max_length=11)
+	
+	def __str__(self):
+		return self.email
