fa86f5c3c5c5ee8aafcc52e9ddb084b36e95e987
Clevinacio <clev.inacio@gmail.com>
2017-11-28 23:58:37 -0300

    Edit em models.py, admin.py e nas páginas do site

--- core/models.py
+++ core/models.py
@@ -15,8 +15,22 @@ class Arte(models.Model):
     dataCadastro = models.DateField('Data de cadastro', auto_now_add=True)
     email = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = 'Arte'
+        verbose_name_plural = 'Artes'
+
 
 class Imagens(models.Model):
     dataImagem = models.DateTimeField("Data da imagem", auto_now_add=True)
     descricao = models.CharField("Descrição", max_length=100, null=True)
-    idArte = models.ForeignKey(Arte, on_delete=models.CASCADE, null = True)
+    idArte = models.ForeignKey(Arte, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = 'Imagem'
+        verbose_name_plural = 'Imagens'
