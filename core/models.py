abd1729e52ffe9f3e233e420750c536527d931d9
Clevinacio <clev.inacio@gmail.com>
2017-11-28 23:41:53 -0300

    Add Arte e Imagens em models.py

--- core/models.py
+++ core/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
+
 class Usuario(models.Model):
-	email= models.CharField('E-mail',primary_key=True, max_length=30)
-	senha= models.CharField('Senha', max_length=16, null=True)
-	cpfCpnj= models.CharField('CPF', max_length=11, null=True)
-		
-	def __str__(self):
-		return self.email
+    email = models.CharField('E-mail', primary_key=True, max_length=30)
+    senha = models.CharField('Senha', max_length=16, null=True)
+    cpfCpnj = models.CharField('CPF', max_length=11, null=True)
+
+    def __str__(self):
+        return self.email
+
 
 class Arte(models.Model):
-	descricao = models.CharField("Descrição", max_length= 100, null=True)
-	dataCadastro = models.DateField('Data de cadastro', auto_now_add=True)
-	email = models.ForeignKey(Usuario, on_delete= models.CASCADE, null=True)
+    descricao = models.CharField("Descrição", max_length=100, null=True)
+    dataCadastro = models.DateField('Data de cadastro', auto_now_add=True)
+    email = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
 
-class ClassName(object):
-	
-		
+
+class Imagens(models.Model):
+    dataImagem = models.DateTimeField("Data da imagem", auto_now_add=True)
+    descricao = models.CharField("Descrição", max_length=100, null=True)
+    idArte = models.ForeignKey(Arte, on_delete=models.CASCADE, null = True)
