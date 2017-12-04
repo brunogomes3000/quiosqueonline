576396fff877581cde59dc10a1b35eacd3b65f02
Anderson-Andre <andre.andersonagl@gmail.com>
2017-12-04 17:40:33 -0300

    adicionar listagem das artes na pagina Resultado busca

--- core/views.py
+++ core/views.py
@@ -3,17 +3,33 @@ from .models import Arte
 
 # Create your views here.
 def index(request):
-	
+	Artes = Arte.objects.all().order_by('-id')[:3]
 	return render(request, 'index.html')
 
 def resultadobuscar(request):
-	Artes = Arte.objects.all()
+	#id_Artes = request.GET.get("id")
+	#Artes = Arte.objects.get(id=id_Artes)
+	#Artes = Arte.objects.all()
+
+	if request.method == 'GET':
+		if 'nomeget' in request.GET:
+			nomeget=request.GET.get("nomeget")
+		else: 
+			nomeget=""
+		artes =  Arte.objects.filter(descricao__icontains=nomeget)
+	else:
+		artes = Arte.objects.all()
+
 	context = {
-	'Artes': Artes
+		'artes': artes
 	}
 
 	return render(request, 'ResultadoBuscar.html', context) 
 
 def arte_detalhes(request):
-		return render(request,'arte_detalhes.html')
+	Artes = Arte.objects.all()
+	context = {
+	'Artes': Artes
+	}
+	return render(request,'arte_detalhes.html', context)
 
