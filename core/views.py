f19a4a689aa4a3ad4860061ed2934521b287db9b
Clevinacio <clev.inacio@gmail.com>
2017-12-20 15:06:56 -0300

    Ultima alteração. Cabou-se

--- core/views.py
+++ core/views.py
@@ -9,7 +9,6 @@ from .forms import UserModelForm
 from .forms import UsuarioModelForm
 from .forms import ArteModelForm
 from .forms import EditArteModelForm
-from .forms import RecuperarModelForm
 from django.core.paginator import Paginator
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
